6ccbd036b3ac2330e7703ea19f856dc544449e9e
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2020-12-04 13:26:13 +0000

    generated tests via vs to see if it makes a difference expanded on fault testing for clamp - ie: minValue>maxValue during ```IntTools.Clamp(value,minValue,maxValue);```

---
+++ SManager.Core.Tests/IntToolsTestSuite.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using SManager.Core;
+
+namespace Test.SManager.Core
+{
+    [TestFixture()]
+    public class IntToolsTestSuite
+    {
+        #region Private Properties
+
+        private int _resultOfClamp { get; set; }
+
+        #endregion Private Properties
+
+        #region Public Methods
+
+        [TestCase(-12, 1, 5)]
+        [TestCase(3, 1, 5)]
+        [TestCase(12, 1, 5)]
+        public void Clamp_ReturnClampedValue(int value, int min, int max)
+        {
+            _resultOfClamp = IntTools.Clamp(value, min, max);
+            if ((_resultOfClamp >= min && _resultOfClamp <= max) || int.Equals(value, _resultOfClamp))
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test()]
+        public void Clamp_ReturnException()
+        {
+            Assert.Throws(Is.TypeOf<System.ArgumentOutOfRangeException>(), () => IntTools.Clamp(3, 5, 1));
+        }
+
+        [SetUp()]
+        public void SetUp()
+        {
+            _resultOfClamp = new int();
+        }
+
+        #endregion Public Methods
+    }
+}
